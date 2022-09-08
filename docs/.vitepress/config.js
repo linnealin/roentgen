65a1f6a09b31c6610421fcabe1d881bf82675bad
linnealin <linnealin96@gmail.om>
2022-09-08 21:18:17 +0200

    deployment

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: 'Radiologi för AT-läkare',
     description: 'Just playing around.',
+    base: '/roentgen/',
     lastUpdated: true,
     themeConfig: {
         nav: [
