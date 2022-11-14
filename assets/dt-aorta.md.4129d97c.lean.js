33884d16f99c1f2580cdd7cf14fb9278596123c1
linnealin <linnealin@users.noreply.github.com>
2022-11-14 18:43:04 +0000

    deploy: f8fb80a8e8d23fecf992b2fc7442c86918a5c020

---
+++ assets/dt-aorta.md.4129d97c.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,o as r,d as e}from"./app.c522a08b.js";const _=JSON.parse('{"title":"DT aorta (flash) med kontrast","description":"","frontmatter":{},"headers":[],"relativePath":"dt-aorta.md","lastUpdated":1668451354000}'),n={name:"dt-aorta.md"},s=e("",5),o=[s];function i(d,l,k,c,m,u){return r(),a("div",null,o)}const g=t(n,[["render",i]]);export{_ as __pageData,g as default};
